1f18b5361d7fe4102c97d34eb66b069347f6d126
busradeniz <busradeniz89@gmail.com>
2018-03-07 23:41:37 +0000

    api layer implementation improved

--- app/src/main/java/com/busradeniz/newsapplication/api/ArticleApi.java
+++ app/src/main/java/com/busradeniz/newsapplication/api/ArticleApi.java
@@ -2,10 +2,12 @@ package com.busradeniz.newsapplication.api;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface ArticleApi {
 
-    @GET("/api/unknown")
-    Call<ArticleListApiResponse> getArticleList();
+    @GET("top-headlines")
+    Call<ArticleListApiResponse> getArticleList(@Query("country") String country, @Query("category") String category,
+                                                @Query("apiKey") String apiKey);
 
 }
--- app/src/main/java/com/busradeniz/newsapplication/api/ArticleService.java
+++ app/src/main/java/com/busradeniz/newsapplication/api/ArticleService.java
@@ -1,13 +1,18 @@
 package com.busradeniz.newsapplication.api;
 
+import retrofit2.Call;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ArticleService {
 
-    private static final String BASE_URL = "https://newsapi.org/v2/top-headlines";
+    private static final String BASE_URL = "https://newsapi.org/v2/";
+    private static final String COUNTRY = "gb";
+    private static final String CATEGORY = "business";
+    private static final String API_KEY = "6b195c9d219f4487a12a02ea8085d798";
     private static ArticleService instance = null;
 
-    private Retrofit retrofit;
+    private ArticleApi articleApi;
 
     public static ArticleService getInstance() {
         if (instance == null) {
@@ -17,10 +22,15 @@ public class ArticleService {
     }
 
     private ArticleService() {
-        retrofit = new Retrofit.Builder()
+        Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
-//                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addConverterFactory(GsonConverterFactory.create())
                 .build();
+        articleApi = retrofit.create(ArticleApi.class);
 
+    }
+
+    public Call<ArticleListApiResponse> getArticles() {
+        return articleApi.getArticleList(COUNTRY, CATEGORY, API_KEY);
     }
 }
