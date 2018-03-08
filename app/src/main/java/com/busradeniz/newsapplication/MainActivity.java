3ed26eaad67cd2d8556aa7e80b87ddcfe86b14e9
busradeniz <busradeniz89@gmail.com>
2018-03-08 00:20:58 +0000

    Recycler view added to layout

--- app/src/main/java/com/busradeniz/newsapplication/MainActivity.java
+++ app/src/main/java/com/busradeniz/newsapplication/MainActivity.java
@@ -1,7 +1,7 @@
 package com.busradeniz.newsapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.busradeniz.newsapplication.api.ArticleListApiResponse;
